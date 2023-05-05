3962fe1bcb0896d825d9802a77ba2ac28adfdd02
Máté Balázs <balmate8@gmail.com>
2023-05-05 15:01:05 +0200

    Add logged-in guard

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -8,15 +8,15 @@ import { SimpleProductComponent } from './simple-product/simple-product.componen
 import { AdminComponent } from './admin/admin.component'
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminGuard } from './guards/admin.guard';
+import { LoggedInGuard } from './guards/logged-in.guard';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}, 
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: '', component: LoginComponent, canActivate: [LoggedInGuard]}, 
+  {path: 'login', component: LoginComponent, canActivate: [LoggedInGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [LoggedInGuard]},
   {path: 'products', component: ProductsComponent, canActivate: [AuthGuard]},
   {path: 'product/:id', component: SimpleProductComponent, canActivate: [AuthGuard]},
   {path: 'admin', component: AdminComponent, canActivate: [AdminGuard]},
-  {path: 'register', component: RegisterComponent},
   {path: 'addProduct', component: AddProductComponent, canActivate: [AdminGuard]}
 ];
 
---
+++ Frontend/src/app/guards/logged-in.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedInGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (localStorage.getItem('username')) {
+        this.router.navigate(['/products']);
+        return false;
+      }
+      return true;
+  }
+  
+}
